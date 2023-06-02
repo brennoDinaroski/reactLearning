03b03b842402feb13cd006f2f035dfd6cf00ab3c
brennoDinaroski <brenno.dinaroski@gmail.com>
2023-06-02 15:05:44 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { User } from "./User.js"
 
 function App() {
-  return (
+  const planets = [
+    { name: "Mars", isGasPlanet: false, mass: 1000},
+    { name: "Earth", isGasPlanet: false, mass: 1500},
+    { name: "Jupiter", isGasPlanet: true, mass: 500},
+    { name: "Venus", isGasPlanet: false, mass: 250},
+    { name: "Neptune", isGasPlanet: true, mass: 1950},
+    { name: "Uranus", isGasPlanet: true, mass: 2100},
+  ]
+
+  return ( 
+    <>
+    <h1 style={{textAlign: "center", color: "blue"}}>Gas Planets</h1>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {planets.map ((planet) => {
+        return ( 
+          planet.mass >= 2000 ?       
+        <GasPlanets name={planet.name} gas={planet.isGasPlanet} key={planet.key} />        
+        : ""
+        )        
+      })}    
     </div>
-  );
+    </>   
+  )
 }
 
-export default App;
+// CREATE A COMPONENT FOR THE USER
+const GasPlanets = (props) => {  
+  return(  
+    <div>
+      <h2 key={props.key}> Name: {props.name}</h2>      
+    </div>  
+  )
+}
+
+
+export default App

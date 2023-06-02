394e8ae72bdf8c9d6a9b8485e5b37eb5f58ae3b3
brennoDinaroski <brenno.dinaroski@gmail.com>
2023-06-02 15:22:52 -0300

    buttons to add, subtract and set to zero

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
 import "./App.css"
-import { User } from "./User.js"
+import { useState } from 'react'
 
 function App() {
-  const planets = [
-    { name: "Mars", isGasPlanet: false, mass: 1000},
-    { name: "Earth", isGasPlanet: false, mass: 1500},
-    { name: "Jupiter", isGasPlanet: true, mass: 500},
-    { name: "Venus", isGasPlanet: false, mass: 250},
-    { name: "Neptune", isGasPlanet: true, mass: 1950},
-    { name: "Uranus", isGasPlanet: true, mass: 2100},
-  ]
+  const [age, setAge] = useState(0)
+
+  const increaseAge = () => {
+    setAge(age + 1)
+  }
+
+  const decreaseAge = () => {
+    setAge(age - 1)
+  }
+ 
+  const setToZero = () => {
+    setAge(0)
+  }
 
   return ( 
-    <>
-    <h1 style={{textAlign: "center", color: "blue"}}>Gas Planets</h1>
     <div className="App">
-      {planets.map ((planet) => {
-        return ( 
-          planet.mass >= 2000 ?       
-        <GasPlanets name={planet.name} gas={planet.isGasPlanet} key={planet.key} />        
-        : ""
-        )        
-      })}    
-    </div>
-    </>   
+      {age}
+      <button  onClick={increaseAge}> Increase Age</button>
+      <button  onClick={decreaseAge}> Increase Age</button>
+      <button  onClick={setToZero}> Increase Age</button>
+      </div>
   )
 }
 
 // CREATE A COMPONENT FOR THE USER
-const GasPlanets = (props) => {  
-  return(  
-    <div>
-      <h2 key={props.key}> Name: {props.name}</h2>      
-    </div>  
-  )
-}
-
-
 export default App

328409cf894ead337a34e71e5e33c98edb002761
brennoDinaroski <brenno.dinaroski@gmail.com>
2023-06-14 22:20:43 -0300

    'useToggle' working with object

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import "./App.css";
 import { useToggle } from "./useToggle";
 
 function App() {  
-  const [isVisible, toggle] = useToggle()
-  const [isVisible2, toggle2] = useToggle()
+  const { state: isVisible, toggle} = useToggle()
+  // const [isVisible2, toggle2] = useToggle()
 
   return(
     <div className="App"> 
-      <button onClick={(toggle)}>
+      <button onClick={toggle}>
         {isVisible ? "Hide" : "Show"}
       </button>    
       {isVisible && <h1>Hidden text</h1>}
 
-      <button onClick={toggle2}>{isVisible2 ? "Hide2" : "Show2"}</button>
-      {isVisible2 && <h1>Hidden Text 2</h1>}
+      {/* <button onClick={toggle2}>{isVisible2 ? "Hide2" : "Show2"}</button>
+      {isVisible2 && <h1>Hidden Text 2</h1>} */}
     </div>
   );  
 }
